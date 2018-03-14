bf8c4113ede7f5650adda2af622fdaeda5137de1
kiosk <kiosk@foundation0.ilt.example.com>
2018-03-14 10:56:31 +0800

    Commit test

---
+++ README.md
@@ -0,0 +1,2 @@
+BK's simple php application that I use to test various things such 
+OpenShift 3.5
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>BK's First PHP Page</title></head>
+
+<body>
+<?php
+echo "<h1>Welcome</h1>"
+
+echo "The date today is " . date("Y-m-d") . <br/>
+?>
+
+</body>
+</html>
